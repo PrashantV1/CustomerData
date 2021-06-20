944f5e6efb2c4ef53ffe5675745061687e2638cc
PrashantV1 <82022271+PrashantV1@users.noreply.github.com>
2021-06-20 13:27:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {
+  View,
+} from "react-native";
+import Customer from "./components/data"
+class App extends React.Component {
+
+  render() {
+    return <View>
+      <Customer/>
+    </View>;
+  }
+}
+
+
+
+export default App;
